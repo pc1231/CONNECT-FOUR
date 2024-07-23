7717daf5bbfdfbdec58e5477c74ba18b48b9deee
Yahia Awad <awadayed12@gmail.com>
2024-07-23 16:03:35 -0400

    function board

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//--------------- constants--------------------------------------
+// constants--------------------------------------
 
 
 
@@ -9,11 +9,35 @@ let board, turn, winner, tie
 
 // Cached Element References
 const squareEls = document.querySelectorAll('.sqr')
-console.log(squareEls);
+const messageEl = document.getElementById('message')
 
+//------Functions--------------
+init()
 
-//---------------Functions--------------
-
-
+function init() {
+// allows us to change turns. after finishing add more need around 42 
+board = ['','X','','','O','','','','']
+turn = 'X'
+winner = false
+tie = false
+render()
+}
+//render displays the state of the game
+function render() {
+updateBoard()
+}
+function updateBoard() {
+    //element inside the space squares
+    // function else if that allows for turns
+    board.forEach((cell, idx) => {  
+            if (cell === 'X') {
+ squareEls[idx].textContent = 'X'
+        } else if (cell === 'O') {
+       squareEls[idx].textContent = 'O'
+        } else { 
+            squareEls[idx].textContent = ''
+        }    
+    })
+}
 
 //-------Event Listeners---------------
