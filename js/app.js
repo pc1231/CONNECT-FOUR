d9e21c69a2b74e7d8ca05275570433b230ec56c6
Yahia Awad <awadayed12@gmail.com>
2024-07-23 10:04:52 -0400

    boxes

---
+++ js/app.js
@@ -0,0 +1,19 @@
+//--------------- constants--------------------------------------
+
+
+
+
+//---------Variables(State)------------------
+let board, turn, winner, tie
+
+
+// Cached Element References
+const squareEls = document.querySelectorAll('.sqr')
+console.log(squareEls);
+
+
+//---------------Functions--------------
+
+
+
+//-------Event Listeners---------------
