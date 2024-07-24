f08257dcb2834359c2c74b4302b9e50f52dbbcad
Yahia Awad <awadayed12@gmail.com>
2024-07-24 09:53:22 -0400

    added a win and tie rule

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ init()
 
 function init() {
 // allows us to change turns. after finishing add more need around 42 
-board = ['','X','','','O','','','','']
+board = ['','X','','X','','','O','','']
 turn = 'X'
 winner = false
 tie = false
@@ -25,6 +25,8 @@ render()
 //render displays the state of the game
 function render() {
 updateBoard()
+updateMessage()
+
 }
 function updateBoard() {
     //element inside the space squares
@@ -32,12 +34,30 @@ function updateBoard() {
     board.forEach((cell, idx) => {  
             if (cell === 'X') {
  squareEls[idx].textContent = 'X'
+ // use this to make the choices change color 
+ // make for connect four 
+ //squareEls[idx].style.backgroundColor = 'red'
         } else if (cell === 'O') {
        squareEls[idx].textContent = 'O'
+      // squareEls[idx].style.backgroundColor = 'blue'
         } else { 
             squareEls[idx].textContent = ''
-        }    
+            squareEls[idx].style.backgroundColor = 'White'
+        }   
     })
 }
+   function updateMessage() {
+    if (!winner &&!tie) {
+        messageEl.textContent = `It is ${turn}'s turn`
+    } // IF THERES A TIE
+    else if (!winner && tie) {
+        messageEl.textContent = 'game over'
+    }
+
+    }
+     // use backticks to make the shorcut instead of putting all 42 turns
+        //render whos turn it is
+      //  !winner = (winner === false)
+
 
 //-------Event Listeners---------------
