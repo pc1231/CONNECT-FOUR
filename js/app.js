40118fd1f9caf5de247481b9fe3a90415626ed5e
Yahia Awad <awadayed12@gmail.com>
2024-07-25 22:57:28 -0400

    horizontal answers and working and connecting four is working

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 // constants--------------------------------------
-    const winningCombos = [
-        // Horizontal
-        [0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6],
-        [7, 8, 9, 10], [8, 9, 10, 11], [9, 10, 11, 12], [10, 11, 12, 13],
-        [14, 15, 16, 17], [15, 16, 17, 18], [16, 17, 18, 19], [17, 18, 19, 20],
-        [21, 22, 23, 24], [22, 23, 24, 25], [23, 24, 25, 26], [24, 25, 26, 27],
-        [28, 29, 30, 31], [29, 30, 31, 32], [30, 31, 32, 33], [31, 32, 33, 34],
-        [35, 36, 37, 38], [36, 37, 38, 39], [37, 38, 39, 40], [38, 39, 40, 41]
-      ];
-      
+const winningCombos = [
+// horizontal
+  [0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, 5],
+    [6, 7, 8, 9], [7, 8, 9, 10], [8, 9, 10, 11],
+    [12, 13, 14, 15], [13, 14, 15, 16], [14, 15, 16, 17],
+    [18, 19, 20, 21], [19, 20, 21, 22], [20, 21, 22, 23],
+    [24, 25, 26, 27], [25, 26, 27, 28], [26, 27, 28, 29],
+    [30, 31, 32, 33], [31, 32, 33, 34], [32, 33, 34, 35],
+    [36, 37, 38, 39], [37, 38, 39, 40], [38, 39, 40, 41],
+];
 // inspect allows us to see the each squares specific number
 //---------Variables(State)------------------
 let board, turn, winner, tie
@@ -18,7 +18,6 @@ const squareEls = document.querySelectorAll('.sqr')
 const messageEl = document.getElementById('message')
 // reset
 const resetBtnEl = document.getElementById('reset')
-console.log(resetBtnEl);
 //------Functions--------------
 init()
 
@@ -26,7 +25,7 @@ function init() {
 // allows us to change turns. after finishing add more need around 42 
 board = ['','','','','','','','','','','','','','','',
 '','','','','','','','','','','','',
-'','','','','','','','','','','','','','','',]
+'','','','','','','','','','','','','','','']
 turn = 'X'
 // false and true without the string makes it a boolean type
 winner = false;
@@ -60,7 +59,8 @@ function updateBoard() {
    function updateMessage() {
     if (!winner &&!tie) {
         messageEl.textContent = `It is ${turn}'s turn`
-    } // IF THERES A TIE
+    } 
+    // IF THERES A TIE
     else if (!winner && tie) {
         messageEl.textContent = 'No Winner '
     // if theres a winner
@@ -93,20 +93,48 @@ placePiece(squareIndex)
         board[index] = turn   
     } 
     // we need 42 check for winner condition for connect four.
-    function checkForWinner() {
-    if ((board[0] !== '' && board[0] === board[1] && board[0] === board[2]) ||
-        (board[3] !== '' && board[3] === board[4] && board[3] === board[5]) ||
-        (board[6] !== '' && board[6] === board[7] && board[6] === board[8]) ||
-        (board[0] !== '' && board[0] === board[3] && board[0] === board[6]) ||
-        (board[1] !== '' && board[1] === board[4] && board[1] === board[7]) ||
-        (board[2] !== '' && board[2] === board[5] && board[2] === board[8]) ||
-        (board[0] !== '' && board[0] === board[4] && board[0] === board[8]) ||
-        (board[2] !== '' && board[2] === board[4] && board[2] === board[6])
-      )
-      {
-      winner = true  
-      }  
+    
+function checkForWinner() {
+    // Hard-coding horizontal winning combinations
+    if (
+      // Row 1
+      (board[0] && board[0] === board[1] && board[0] === board[2] && board[0] === board[3]) ||
+      (board[1] && board[1] === board[2] && board[1] === board[3] && board[1] === board[4]) ||
+      (board[2] && board[2] === board[3] && board[2] === board[4] && board[2] === board[5]) ||
+  
+      // Row 2
+      (board[6] && board[6] === board[7] && board[6] === board[8] && board[6] === board[9]) ||
+      (board[7] && board[7] === board[8] && board[7] === board[9] && board[7] === board[10]) ||
+      (board[8] && board[8] === board[9] && board[8] === board[10] && board[8] === board[11]) ||
+  
+      // Row 3
+      (board[12] && board[12] === board[13] && board[12] === board[14] && board[12] === board[15]) ||
+      (board[13] && board[13] === board[14] && board[13] === board[15] && board[13] === board[16]) ||
+      (board[14] && board[14] === board[15] && board[14] === board[16] && board[14] === board[17]) ||
+  
+      // Row 4
+      (board[18] && board[18] === board[19] && board[18] === board[20] && board[18] === board[21]) ||
+      (board[19] && board[19] === board[20] && board[19] === board[21] && board[19] === board[22]) ||
+      (board[20] && board[20] === board[21] && board[20] === board[22] && board[20] === board[23]) ||
+  
+      // Row 5
+      (board[24] && board[24] === board[25] && board[24] === board[26] && board[24] === board[27]) ||
+      (board[25] && board[25] === board[26] && board[25] === board[27] && board[25] === board[28]) ||
+      (board[26] && board[26] === board[27] && board[26] === board[28] && board[26] === board[29]) ||
+  
+      // Row 6
+      (board[30] && board[30] === board[31] && board[30] === board[32] && board[30] === board[33]) ||
+      (board[31] && board[31] === board[32] && board[31] === board[33] && board[31] === board[34]) ||
+      (board[32] && board[32] === board[33] && board[32] === board[34] && board[32] === board[35]) ||
+  
+      // Row 7
+      (board[36] && board[36] === board[37] && board[36] === board[38] && board[36] === board[39]) ||
+      (board[37] && board[37] === board[38] && board[37] === board[39] && board[37] === board[40]) ||
+      (board[38] && board[38] === board[39] && board[38] === board[40] && board[38] === board[41])
+    ) {
+      winner = true;
     }
+  }
     //  checking for tie function
     function checkForTie() {
     if (winner) {
