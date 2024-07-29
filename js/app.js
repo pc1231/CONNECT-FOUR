4c12c00f66179c0b1f77352f0f72ad8e94e50ad7
Yahia Awad <awadayed12@gmail.com>
2024-07-29 12:47:22 -0400

    removed hard code for a loop and touched up the read me file

--- js/app.js
+++ js/app.js
@@ -38,8 +38,8 @@ const winningCombos = [
       '', '', '', '', '', '', '', ''
     ];
     turn = 'X';
-    winner = false;
-    tie = false;
+    winner = false
+    tie = false
     render();
   }
   
@@ -90,75 +90,11 @@ const winningCombos = [
   }
   
   function checkForWinner() {
-    if (
-      (board[0] && board[0] === board[1] && board[0] === board[2] && board[0] === board[3]) ||
-      (board[1] && board[1] === board[2] && board[1] === board[3] && board[1] === board[4]) ||
-      (board[2] && board[2] === board[3] && board[2] === board[4] && board[2] === board[5]) ||
-      (board[6] && board[6] === board[7] && board[6] === board[8] && board[6] === board[9]) ||
-      (board[7] && board[7] === board[8] && board[7] === board[9] && board[7] === board[10]) ||
-      (board[8] && board[8] === board[9] && board[8] === board[10] && board[8] === board[11]) ||
-      (board[12] && board[12] === board[13] && board[12] === board[14] && board[12] === board[15]) ||
-      (board[13] && board[13] === board[14] && board[13] === board[15] && board[13] === board[16]) ||
-      (board[14] && board[14] === board[15] && board[14] === board[16] && board[14] === board[17]) ||
-      (board[18] && board[18] === board[19] && board[18] === board[20] && board[18] === board[21]) ||
-      (board[19] && board[19] === board[20] && board[19] === board[21] && board[19] === board[22]) ||
-      (board[20] && board[20] === board[21] && board[20] === board[22] && board[20] === board[23]) ||
-      (board[24] && board[24] === board[25] && board[24] === board[26] && board[24] === board[27]) ||
-      (board[25] && board[25] === board[26] && board[25] === board[27] && board[25] === board[28]) ||
-      (board[26] && board[26] === board[27] && board[26] === board[28] && board[26] === board[29]) ||
-      (board[30] && board[30] === board[31] && board[30] === board[32] && board[30] === board[33]) ||
-      (board[31] && board[31] === board[32] && board[31] === board[33] && board[31] === board[34]) ||
-      (board[32] && board[32] === board[33] && board[32] === board[34] && board[32] === board[35]) ||
-      (board[36] && board[36] === board[37] && board[36] === board[38] && board[36] === board[39]) ||
-      (board[37] && board[37] === board[38] && board[37] === board[39] && board[37] === board[40]) ||
-      (board[38] && board[38] === board[39] && board[38] === board[40] && board[38] === board[41]) ||
-      (board[0] && board[0] === board[6] && board[0] === board[12] && board[0] === board[18]) ||
-      (board[6] && board[6] === board[12] && board[6] === board[18] && board[6] === board[24]) ||
-      (board[12] && board[12] === board[18] && board[12] === board[24] && board[12] === board[30]) ||
-      (board[1] && board[1] === board[7] && board[1] === board[13] && board[1] === board[19]) ||
-      (board[7] && board[7] === board[13] && board[7] === board[19] && board[7] === board[25]) ||
-      (board[13] && board[13] === board[19] && board[13] === board[25] && board[13] === board[31]) ||
-      (board[2] && board[2] === board[8] && board[2] === board[14] && board[2] === board[20]) ||
-      (board[8] && board[8] === board[14] && board[8] === board[20] && board[8] === board[26]) ||
-      (board[14] && board[14] === board[20] && board[14] === board[26] && board[14] === board[32]) ||
-      (board[3] && board[3] === board[9] && board[3] === board[15] && board[3] === board[21]) ||
-      (board[9] && board[9] === board[15] && board[9] === board[21] && board[9] === board[27]) ||
-      (board[15] && board[15] === board[21] && board[15] === board[27] && board[15] === board[33]) ||
-      (board[4] && board[4] === board[10] && board[4] === board[16] && board[4] === board[22]) ||
-      (board[10] && board[10] === board[16] && board[10] === board[22] && board[10] === board[28]) ||
-      (board[16] && board[16] === board[22] && board[16] === board[28] && board[16] === board[34]) ||
-      (board[5] && board[5] === board[11] && board[5] === board[17] && board[5] === board[23]) ||
-      (board[11] && board[11] === board[17] && board[11] === board[23] && board[11] === board[29]) ||
-      (board[17] && board[17] === board[23] && board[17] === board[29] && board[17] === board[35]) ||
-      (board[18] && board[18] === board[24] && board[18] === board[30] && board[18] === board[36]) ||
-      (board[19] && board[19] === board[25] && board[19] === board[31] && board[19] === board[37]) ||
-      (board[20] && board[20] === board[26] && board[20] === board[32] && board[20] === board[38]) ||
-      (board[21] && board[21] === board[27] && board[21] === board[33] && board[21] === board[39]) ||
-      (board[22] && board[22] === board[28] && board[22] === board[34] && board[22] === board[40]) ||
-      (board[23] && board[23] === board[29] && board[23] === board[35] && board[23] === board[41]) ||
-      (board[18] && board[18] === board[13] && board[18] === board[8] && board[18] === board[3]) ||
-      (board[19] && board[19] === board[14] && board[19] === board[9] && board[19] === board[4]) ||
-      (board[20] && board[20] === board[15] && board[20] === board[10] && board[20] === board[5]) ||
-      (board[24] && board[24] === board[19] && board[24] === board[14] && board[24] === board[9]) ||
-      (board[25] && board[25] === board[20] && board[25] === board[15] && board[25] === board[10]) ||
-      (board[26] && board[26] === board[21] && board[26] === board[16] && board[26] === board[11]) ||
-      (board[30] && board[30] === board[25] && board[30] === board[20] && board[30] === board[15]) ||
-      (board[31] && board[31] === board[26] && board[31] === board[21] && board[31] === board[16]) ||
-      (board[32] && board[32] === board[27] && board[32] === board[22] && board[32] === board[17]) ||
-      (board[0] && board[0] === board[7] && board[0] === board[14] && board[0] === board[21]) ||
-      (board[1] && board[1] === board[8] && board[1] === board[15] && board[1] === board[22]) ||
-      (board[2] && board[2] === board[9] && board[2] === board[16] && board[2] === board[23]) ||
-      (board[6] && board[6] === board[13] && board[6] === board[20] && board[6] === board[27]) ||
-      (board[7] && board[7] === board[14] && board[7] === board[21] && board[7] === board[28]) ||
-      (board[8] && board[8] === board[15] && board[8] === board[22] && board[8] === board[29]) ||
-      (board[12] && board[12] === board[19] && board[12] === board[26] && board[12] === board[33]) ||
-      (board[13] && board[13] === board[20] && board[13] === board[27] && board[13] === board[34]) ||
-      (board[14] && board[14] === board[21] && board[14] === board[28] && board[14] === board[35]) ||
-      (board[18] && board[18] === board[25] && board[18] === board[32] && board[18] === board[39]) ||
-      (board[19] && board[19] === board[26] && board[19] === board[33] && board[19] === board[40]) ||
-      (board[20] && board[20] === board[27] && board[20] === board[34] && board[20] === board[41])
-    ) {
-      winner = true;
+    for (const combo of winningCombos) {
+      const [a, b, c, d] = combo;
+      if (board[a] && board[a] === board[b] && board[a] === board[c] && board[a] === board[d]) {
+        winner = true;  
+      }
     }
   }
   
@@ -178,5 +114,4 @@ const winningCombos = [
     squareEl.addEventListener('click', handleClick);
   });
   resetBtnEl.addEventListener('click', init);
-  
   
